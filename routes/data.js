5f4b5a97df0e6e7b3de6f2d270dd61ba1d71526a
Jeff Steward <steward.jeff@gmail.com>
2017-05-15 21:56:15 -0400

    initial version

---
+++ routes/data.js
@@ -0,0 +1,54 @@
+var request = require('request');
+var express = require('express');
+var router = express.Router();
+
+var apikey = process.env.APIKEY;
+var apiURL = "http://api.harvardartmuseums.org/experimental/object";
+
+
+router.get('/terms/sample', function(req, res, next) {
+	res.sent("sample set");
+});
+
+/* GET users listing. */
+router.get('/terms/:term', function(req, res, next) {
+
+	var term = req.params.term;
+	if (term) {
+
+		request(apiURL, {
+				qs: {
+					apikey: apikey, 
+					q: 'images.googlevision.responses.textAnnotations.description:' + term,
+					fields: 'images.googlevision.responses.textAnnotations',
+					sort: 'random',
+					size: 100
+				}
+			}, function(error, response, body) {
+				var r = JSON.parse(body);
+				var output = [];
+
+				if (!r.error) {
+					for (var i = 0; i < r.records.length; i++) {
+						text = r.records[i].images[0].googlevision.responses[0].textAnnotations;
+						text.forEach(function(t) {
+							if (t.description.toUpperCase() === term.toUpperCase()) {
+								output.push(t);
+							}
+						});
+					}
+				} else {
+
+				}
+
+				res.send(output);
+			});
+
+	} else {
+		res.send("no term");
+	}
+
+
+});
+
+module.exports = router;
