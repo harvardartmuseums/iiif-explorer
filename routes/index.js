71648b6dc37e211fc64680a1f363760145996ab6
Jeff Steward <steward.jeff@gmail.com>
2017-05-17 15:17:39 -0400

    reorganize the routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'IIIF Stickers' });
 });
 
+router.get('/note-generator', function(req, res, next) {
+  res.render('note-generator', { title: 'IIIF Stickers' });
+});
+
+router.get('/faces/compare', function(req, res, next) {
+  res.render('face-compare', { title: 'IIIF Stickers' });
+});
+
+
 module.exports = router;
