5ba52d6eb398d66eaf2db0aecbc7a4e56424495c
Jeff Steward <steward.jeff@gmail.com>
2017-05-19 16:36:22 -0400

    add links to the main page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IIIF Stickers' });
+  res.render('index', { title: 'IIIF Explorer' });
 });
 
 router.get('/note-generator', function(req, res, next) {
